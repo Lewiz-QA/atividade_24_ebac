97465c0ce2b7782d05827214192b825003ebbfcb
Luiz <lgm97.com@gmail.com>
2023-11-03 00:14:38 -0300

    Testes de API com Supertest

--- sample-supertest-api-testing/utils/request.js
+++ sample-supertest-api-testing/utils/request.js
@@ -13,4 +13,32 @@ let getAccessToken = (user, pass) => {
         })
 }
 
-module.exports = { getAccessToken }
+let getAddressId = (token) => {
+    return req('http://localhost:3000/api')
+        .post('/addresses')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            "address_1": "Logradouro Teste",
+            "address_2": "Bairro Teste",
+            "city": "Cidadade Teste",
+            "state": "UF Teste",
+            "zip": Math.floor(Math.random().toString().slice(2,10))
+        })
+        .set('Accept', 'application/json')
+        .then(response => {
+            return response.body.id
+        })
+}
+
+let deleteAddress = (token, addressId) => {
+    return req('http://localhost:3000/api')
+        .delete(`/addresses/${addressId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .set('Accept', 'application/json')
+        .then(response => {
+            expect(response.statusCode).toEqual(200)
+        })
+}
+
+
+module.exports = { getAccessToken, getAddressId, deleteAddress }
